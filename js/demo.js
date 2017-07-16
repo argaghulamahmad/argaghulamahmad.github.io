4ef6a9b4bcc7e7c0c09aaee713f8e486a3a8e430
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2017-07-16 18:15:18 +0700

    re add lost file (CNAME and README)

--- js/demo.js
+++ js/demo.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 	"use strict";
 	particleground(document.getElementById('particles-ground'), {
-		minSpeedX: 0.1,
-        maxSpeedX: 0.7,
-        minSpeedY: 0.1,
-        maxSpeedY: 0.7,
-        directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
-        directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
-        density: 10000, // How many particles will be generated: one particle every n pixels
+		density: 10000, // How many particles will be generated: one particle every n pixels
         dotColor: '#666666',
         lineColor: '#666666',
         particleRadius: 9, // Dot size
@@ -17,11 +11,3 @@ document.addEventListener('DOMContentLoaded', function () {
         parallax: true
 	});
 }, false);
-
-//document.addEventListener('DOMContentLoaded', function () {
-//	"use strict";
-//	particleground(document.getElementById('particles-ground'), {
-//		dotColor: '#5cbdaa',
-//		lineColor: '#5cbdaa'
-//	});
-//}, false);
