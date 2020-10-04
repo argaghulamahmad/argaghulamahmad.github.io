81b4c5a63b07b7fe4412cfb46d50c1cd145aae2e
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2020-10-05 06:20:58 +0700

    Change contents

--- js/main.js
+++ js/main.js
@@ -1,189 +1,64 @@
 //Use Strict Mode
 (function ($) {
-	"use strict";
+    "use strict";
 
-	//Begin - Window Load
-	$(window).load(function () {
-
-
-		//==============___Page Loader___================
-
-		$('#page-loader').delay(300).fadeOut(400, function () {
-
-		});
-
-		$('#loader-name').addClass('loader-left');
-		$('#loader-job').addClass('loader-right');
-		$('#loader-animation').addClass('loader-hide');
-
-	});
-
-	//Begin - Document Ready
-	$(document).ready(function () {
-		/*profile target animation*/
-		var profile_target = $('h2[id^="profile-target-"]').hide(),
-			i = 0;
-
-		(function cycle() {
-
-			profile_target.eq(i).fadeIn(400)
-				.delay(5000)
-				.fadeOut(400, cycle);
-
-			i = ++i % profile_target.length;
-
-		})();
-
-		//==============___Page Loader___================
-		$('#loading-wraper').fadeIn(300);
-
-		//==============___Testimonials - owl Carousel___================
-		$("#testimonial-carousel").owlCarousel({
-			navigation: false, // Show next and prev buttons
-			slideSpeed: 300,
-			paginationSpeed: 400,
-			responsiveRefreshRate: 200,
-			responsiveBaseWidth: window,
-			pagination: true,
-			singleItem: true,
-			navigationText: ["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
-		});
+    //Begin - Window Load
+    $(window).load(function () {
 
 
-		//==============_Map_================
-		$('.map').on('click', function () {
-			$('.map-overlay').hide();
-		});
+        //==============___Page Loader___================
 
-		$('.map').on('mouseleave', function () {
-			$('.map-overlay').show();
-		});
+        $('#page-loader').delay(300).fadeOut(400, function () {
 
-		//==============_Lightbox_================
-		//Nivo Lightbox
-		$('a.nivobox').nivoLightbox({
-			effect: 'fade'
-		});
+        });
 
+        $('#loader-name').addClass('loader-left');
+        $('#loader-job').addClass('loader-right');
+        $('#loader-animation').addClass('loader-hide');
 
-		//==============___Scrollbars___================
-		$('.section-vcardbody').perfectScrollbar({
-			wheelSpeed: 0.9
-		});
+    });
 
-		//==============___Menu & Pages Animation___================
+    //Begin - Document Ready
+    $(document).ready(function () {
+        /*profile target animation*/
+        var profile_target = $('h2[id^="profile-target-"]').hide(),
+            i = 0;
 
-		var linkHome = 0;
-		var linkPage = '';
+        (function cycle() {
 
-		function pageOn() {
-			$('#main-menu').addClass('main-menu-pgactive');
-			$('#section-home').addClass('section-vcardbody-pgactive');
-			$('.profileActive').removeClass('profileActive');
-			$('#profile2').addClass('profileActive');
+            profile_target.eq(i).fadeIn(400)
+                .delay(5000)
+                .fadeOut(400, cycle);
 
-			linkHome = 1;
-		}
+            i = ++i % profile_target.length;
 
-		function pageOff() {
-			$('.section-page-active').removeClass('section-page-active');
-			$('#main-menu').removeClass('main-menu-pgactive');
-			$('#section-home').removeClass('section-vcardbody-pgactive');
-			$('.profileActive').removeClass('profileActive');
-			$('#profile1').addClass('profileActive');
-			linkHome = 0;
-		}
+        })();
 
+        //==============___Page Loader___================
+        $('#loading-wraper').fadeIn(300);
 
-		$(".link-page").on('click', function (event) {
-			event.preventDefault();
-			$('.menuActive').removeClass('menuActive');
-			$(this).addClass('menuActive');
-			linkPage = $(this).attr('href');
-			$('.section-page-active').removeClass('section-page-active');
-			$(linkPage).addClass('section-page-active');
-			pageOn();
-		});
+        //==============___Testimonials - owl Carousel___================
+        $("#testimonial-carousel").owlCarousel({
+            navigation: false, // Show next and prev buttons
+            slideSpeed: 300,
+            paginationSpeed: 400,
+            responsiveRefreshRate: 200,
+            responsiveBaseWidth: window,
+            pagination: true,
+            singleItem: true,
+            navigationText: ["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
+        });
+
+        //==============_Lightbox_================
+        //Nivo Lightbox
+        $('a.nivobox').nivoLightbox({
+            effect: 'fade'
+        });
 
 
-		$(".link-home").on('click', function (event) {
-			event.preventDefault();
-
-			if (linkHome === 0) {
-				//pageOn();
-			} else if (linkHome === 1) {
-				$('.menuActive').removeClass('menuActive');
-				$(this).addClass('menuActive');
-				pageOff();
-			}
-		});
-
-		//==============___Blog - Ajax___================
-		function loadPost() {
-			$.ajax({
-				url: 'single.html', // URL HERE
-				type: 'GET',
-				success: function (html) {
-
-					var $lis = $(html).find('#blogPost'); // Loads the content inside #blogPost div
-
-					$("#postHere").html($lis);
-				}
-			});
-		}
-
-		$(".loadPost").on('click', function (event) {
-			event.preventDefault();
-			//$("#postHere").html('loading...');
-			$('.section-page-active').removeClass('section-page-active');
-			$('#page-blog-single').addClass('section-page-active');
-			pageOn();
-			loadPost();
-		});
-
-		//		//==============___Contact Form Validator and Ajax Sender___================
-		//		$("#contactForm").validate({
-		//			submitHandler: function (form) {
-		//				$.ajax({
-		//					type: "POST",
-		//					url: "php/contact-form.php",
-		//					data: {
-		//						"name": $("#contactForm #name").val(),
-		//						"email": $("#contactForm #email").val(),
-		//						"subject": $("#contactForm #subject").val(),
-		//						"message": $("#contactForm #message").val()
-		//					},
-		//					dataType: "json",
-		//					success: function (data) {
-		//						if (data.response == "success") {
-		//							$("#contactSuccess").fadeIn(300);
-		//							$("#contactError").addClass("hidden");
-		//
-		//							$("#contactForm #name, #contactForm #email, #contactForm #subject, #contactForm #message")
-		//								.val("")
-		//								.blur()
-		//								.closest(".control-group")
-		//								.removeClass("success")
-		//								.removeClass("error");
-		//
-		//						} else {
-		//							$("#contactError").fadeIn(300);
-		//							$("#contactSuccess").addClass("hidden");
-		//						}
-		//					}
-		//
-		//				});
-		//			}
-		//		});
-
-
-		//Modal for Contact Form
-		//		$('.modal-wrap').click(function () {
-		//			$('.modal-wrap').fadeOut(300);
-		//		});
-
-		//End - Document Ready
-	});
-
-	//End - Use Strict mode
+        //==============___Scrollbars___================
+        $('.section-vcardbody').perfectScrollbar({
+            wheelSpeed: 0.9
+        });
+    });
 })(jQuery);
